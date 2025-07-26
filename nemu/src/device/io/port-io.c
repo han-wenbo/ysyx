c6847b709ee0873ef9df33f18f4a8d4fc49a5ed8
tracer-ysyx <tracer@ysyx.org>
2025-07-26 23:43:02 +0800

    > compile NEMU ysyx_22040000 Wenbo Han Linux VM-20-15-ubuntu 6.8.0-51-generic #52-Ubuntu SMP PREEMPT_DYNAMIC Thu Dec  5 13:09:44 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:43:02 up 2 days,  5:56,  4 users,  load average: 0.15, 0.03, 0.01

--- nemu/src/device/io/port-io.c
+++ nemu/src/device/io/port-io.c
@@ -22,6 +22,8 @@ static IOMap maps[NR_MAP] = {};
 static int nr_map = 0;
 
 /* device interface */
+// space : <-- malloc
+// addr  : <-- mmio address
 void add_pio_map(const char *name, ioaddr_t addr, void *space, uint32_t len, io_callback_t callback) {
   assert(nr_map < NR_MAP);
   assert(addr + len <= PORT_IO_SPACE_MAX);
