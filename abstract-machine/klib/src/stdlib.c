fc46bbccac2bcc749121765ae51d790a0905ff35
tracer-ysyx <tracer@ysyx.org>
2025-07-29 05:28:35 +0800

    > compile NEMU ysyx_22040000 Wenbo Han Linux VM-20-15-ubuntu 6.8.0-51-generic #52-Ubuntu SMP PREEMPT_DYNAMIC Thu Dec  5 13:09:44 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  05:28:35 up 4 days, 11:41,  5 users,  load average: 0.89, 0.61, 0.28

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -37,22 +37,15 @@ void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
-  printf("Allocate %d bytes, strating from 0x%x\n", size,_addr);
-    if (size == 0) return NULL;
+#if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
+  if (size == 0) return NULL;
   void * __addr = _addr;
   size = (size + (__BLOCK_SIZE - 1)) & ~__BLOCK_MASK;
   _addr += size;
   printf("Allocate %d bytes, strating from 0x%x\n", size, __addr);
   return __addr;
-
-#if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
- /* if (size == 0) return NULL;
-  void * __addr = _addr;
-  size = (size + (__BLOCK_SIZE - 1)) & ~__BLOCK_MASK;
-  _addr += size;
-  printf("Allocate %d bytes, strating from 0x%x\n", size, __addr);
-  return __addr;*/
 #endif
+  return NULL;
 }
 
 void free(void *ptr) {
