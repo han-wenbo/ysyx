56342d61bcb0158fa252fd08ad1c5ecb7ce6c19f
tracer-ysyx <tracer@ysyx.org>
2025-07-30 06:35:24 +0800

    > compile NEMU ysyx_22040000 Wenbo Han Linux VM-20-15-ubuntu 6.8.0-51-generic #52-Ubuntu SMP PREEMPT_DYNAMIC Thu Dec  5 13:09:44 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  06:35:24 up 5 days, 12:48,  5 users,  load average: 0.00, 0.03, 0.01

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -3,6 +3,7 @@
 
 //  #define VGACTL_ADDR     (DEVICE_BASE + 0x0000100)
 #define SYNC_ADDR (VGACTL_ADDR + 4)
+//#define FB_ADDR 0xa1000000
 
 extern void __am_gpu_config(AM_GPU_CONFIG_T *cfg);
 void __am_gpu_init() {
@@ -24,12 +25,27 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
     .width = regv >> 16, .height = (regv & 0x0000FFFF) ,
-    .vmemsz = 0x100
+    .vmemsz = (regv >> 16) * (regv & 0x0000FFFF) * 4
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+
+  AM_GPU_CONFIG_T cfg;
+ __am_gpu_config(&cfg); 
+
+//  int w = cfg.width;  // TODO: get the correct width
+  int h = cfg.height;  // TODO: get the correct height
+ 
+  int (*fb_addr)[h] = (int (*)[h])FB_ADDR;
+  int (*pix)[ctl->h] = ctl->pixels;
+
   if (ctl->sync) {
+     for(int i = 0; i < ctl->w; i++) {
+        for(int j = 0; j < ctl->h; j++){
+	   outl((int)(&fb_addr[ctl->x+i][ctl->y+j]), pix[i][j]);
+	}
+     }	     
     outl(SYNC_ADDR, 1);
   }
 }
