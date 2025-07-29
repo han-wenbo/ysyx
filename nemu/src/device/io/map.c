c74fdd4bdfca69e59d1e7e13b39eaf815cc91624
tracer-ysyx <tracer@ysyx.org>
2025-07-29 23:22:21 +0800

    > compile NEMU ysyx_22040000 Wenbo Han Linux VM-20-15-ubuntu 6.8.0-51-generic #52-Ubuntu SMP PREEMPT_DYNAMIC Thu Dec  5 13:09:44 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:22:21 up 5 days,  5:35,  5 users,  load average: 1.79, 1.53, 1.48

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -57,7 +57,9 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
   invoke_callback(map->callback, offset, len, false); // prepare data to read
+#ifdef CONFIG_DTRACE
   dtrace_write("PC: 0x%x, read %s (0x%x)\n", cpu.pc, map->name, addr);
+#endif
   word_t ret = host_read(map->space + offset, len);
   return ret;
 }
@@ -67,6 +69,8 @@ void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
+#ifdef CONFIG_DTRACE
   dtrace_write("PC: 0x%x, write %s (0x%x)\n", cpu.pc, map->name, addr);
+#endif
   invoke_callback(map->callback, offset, len, true);
 }
