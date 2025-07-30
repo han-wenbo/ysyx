ca8e34eba753e8316e56f8b443da7b0d02efdc59
tracer-ysyx <tracer@ysyx.org>
2025-07-30 22:04:41 +0800

    > compile NEMU ysyx_22040000 Wenbo Han Linux VM-20-15-ubuntu 6.8.0-51-generic #52-Ubuntu SMP PREEMPT_DYNAMIC Thu Dec  5 13:09:44 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:04:41 up 6 days,  4:17,  5 users,  load average: 0.00, 0.02, 0.05

--- npc/csrc/sim_mem.cpp
+++ npc/csrc/sim_mem.cpp
@@ -10,11 +10,12 @@ static unsigned long long last_time = 0000000000;
 static int r;
 static int addr;
 extern "C" int dpi_pmem_read(uint32_t raddr) {
+	/*
         if(last_time == ttime) {
           // printf("Repeatlly access addr:0x%x,data:0x%x\n" ,raddr,r);
 	   return r;	
 	}
-	last_time = ttime;
+	last_time = ttime; */
 	r = paddr_read(raddr, 4);
 	//printf("read addr:%x, data:%x\n", raddr,r);
 	return (int) r;
