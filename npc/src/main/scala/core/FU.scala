db6ccb52325a8310ba6b96d9d6578e76dc1f0e71
tracer-ysyx <tracer@ysyx.org>
2025-07-31 03:05:55 +0800

    > compile NEMU ysyx_22040000 Wenbo Han Linux VM-20-15-ubuntu 6.8.0-51-generic #52-Ubuntu SMP PREEMPT_DYNAMIC Thu Dec  5 13:09:44 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  03:05:55 up 6 days,  9:18,  6 users,  load average: 0.74, 0.51, 0.40

--- npc/src/main/scala/core/FU.scala
+++ npc/src/main/scala/core/FU.scala
@@ -17,8 +17,7 @@ class ImemDpi extends BlackBox with HasBlackBoxInline {
       |  output reg  [31:0] inst
       |);
       | always @(*) $$display(" en=%b addr=%h", en, addr);
-      | wire [31:0] data = dpi_pmem_read(addr);
-      | assign inst = en ? data : 32'h0000_0013;
+      |   assign inst = en ? dpi_pmem_read(addr) : 32'h0000_0013;
       |endmodule
       |""".stripMargin)
 }
