54c2ca97298d6dc80c46474e37ef09abb0496649
tracer-ysyx <tracer@ysyx.org>
2025-07-31 03:04:00 +0800

    > compile NEMU ysyx_22040000 Wenbo Han Linux VM-20-15-ubuntu 6.8.0-51-generic #52-Ubuntu SMP PREEMPT_DYNAMIC Thu Dec  5 13:09:44 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  03:04:00 up 6 days,  9:17,  6 users,  load average: 0.34, 0.42, 0.37

--- npc/vsrc/ImemDpi.v
+++ npc/vsrc/ImemDpi.v
@@ -5,10 +5,6 @@ module ImemDpi(
   output reg  [31:0] inst
 );
  always @(*) $display(" en=%b addr=%h", en, addr);
-  always @(*) begin
-    if (en)
-      inst = dpi_pmem_read(addr);   
-    else
-      inst = 32'h0000_0013;         
-  end
+ wire [31:0] data = dpi_pmem_read(addr);
+ assign inst = en ? data : 32'h0000_0013;
 endmodule
