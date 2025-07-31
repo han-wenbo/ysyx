ba44723d30993ba844fa89c004eac89c42a83c34
tracer-ysyx <tracer@ysyx.org>
2025-08-01 03:25:23 +0800

    > compile NEMU ysyx_22040000 Wenbo Han Linux VM-20-15-ubuntu 6.8.0-51-generic #52-Ubuntu SMP PREEMPT_DYNAMIC Thu Dec  5 13:09:44 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  03:25:23 up 7 days,  9:38,  6 users,  load average: 0.22, 0.05, 0.02

--- npc/src/main/resources/DpiMem.sv
+++ npc/src/main/resources/DpiMem.sv
@@ -3,23 +3,26 @@ import "DPI-C" function void dpi_pmem_write(input int waddr,
                                         input int wdata,
                                         input byte wmask);
 
-module MemContrl (
-    input  logic        valid,
-    input  logic [31:0] raddr,
-    input  logic        wen,
-    input  logic [31:0] waddr,
-    input  logic [31:0] wdata,
-    input  logic  [7:0] wmask,
-    output logic [31:0] rdata
+
+module MemContrl(
+    input         valid,
+    input         wen,
+    input  [31:0] raddr,
+    input  [31:0] waddr,
+    input  [31:0] wdata,
+    input  [7:0]  wmask,
+    output reg [31:0] rdata
 );
 
-  always @(*) begin
+always @(*) begin
     if (valid) begin
-      rdata = dpi_pmem_read(raddr);
-      if (wen) dpi_pmem_write(waddr, wdata, wmask);
+        rdata = dpi_pmem_read(raddr);
+        if (wen) begin
+            dpi_pmem_write(waddr, wdata, wmask);
+        end
     end else begin
-      rdata = 32'b0;
+        rdata = 32'b0;
     end
-  end
-endmodule
+end
 
+endmodule
