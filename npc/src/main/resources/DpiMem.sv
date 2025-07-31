bec75632248da6b3fca1b4d400921b3e5b2c6987
tracer-ysyx <tracer@ysyx.org>
2025-08-01 04:26:04 +0800

    > compile NEMU ysyx_22040000 Wenbo Han Linux VM-20-15-ubuntu 6.8.0-51-generic #52-Ubuntu SMP PREEMPT_DYNAMIC Thu Dec  5 13:09:44 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  04:26:04 up 7 days, 10:39,  6 users,  load average: 0.29, 0.16, 0.15

--- npc/src/main/resources/DpiMem.sv
+++ npc/src/main/resources/DpiMem.sv
@@ -15,7 +15,7 @@ module MemContrl(
     output reg [31:0] rdata
 );
 
-always @(valid or wen  or wdata ) begin
+always @(valid or wen  or  wmask) begin
     if (valid) begin
         rdata = dpi_pmem_read(raddr);
         if (wen) begin
